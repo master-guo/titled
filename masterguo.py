244b982208162e68d4491fa418d21f30660291a8
masterguo <13913957501@qq.com>
2020-04-03 11:30:49 +0800

    Initial commit

---
+++ masterguo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding:utf-8
+from scapy.all import *
+print(dir(IP.layers))
+print(dir(IP.layers))
+print(dir(IP.layers))
+# ping_one_reply = sr1(all.IP(dst = '192.168.18.2')/all.ICMP(),timeout = 1)
+# ping_one_reply.show()
+
+
+
