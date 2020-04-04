b09171d16e295f2bc048e775cc2b21a690c30b04
masterguo <13913957501@qq.com>
2020-04-04 09:14:14 +0800

    v1

--- masterguo.py
+++ masterguo.py
@@ -4,6 +4,7 @@ from scapy.all import *
 print(dir(IP.layers))
 print(dir(IP.layers))
 print(dir(IP.layers))
+print(dir(IP.layers))
 # ping_one_reply = sr1(all.IP(dst = '192.168.18.2')/all.ICMP(),timeout = 1)
 # ping_one_reply.show()
 
